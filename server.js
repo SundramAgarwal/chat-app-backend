61025bc0fcffd2a16ffe448bc34a5bf4ac565bd3
Sundram Agarwal <agarwal.sundaram@gmail.com>
2023-08-23 12:46:17 +0530

    commit

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.get("/api/chats", (req, res) => {
   res.send(chats);
 });
 
-// app.get("/api/chat/:id", (req, res) => {
-//   const singleChat = chats.find((c) => c._id === req.params.id);
-//   res.send(singleChat);
-// });
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
 
 const PORT = process.env.PORT || 5000;
 mongoose
