af5b82fff07a341e05f64107b2bea452eea12eeb
Sundram Agarwal <agarwal.sundaram@gmail.com>
2023-08-23 12:43:39 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose"); //imported all dependencies
+mongoose.set("strictQuery", false);
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { chats } = require("./data/data");
+
+const app = express();
+
+app.use(express.json());
+// app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      // "https://chat-app.vercel.app"
+    ],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!!");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+// app.get("/api/chat/:id", (req, res) => {
+//   const singleChat = chats.find((c) => c._id === req.params.id);
+//   res.send(singleChat);
+// });
+
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on PORT: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
